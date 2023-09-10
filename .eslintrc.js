609760a3ac7215aa88f5a4b95e715a329febbaed
Łukasz Kopyto <lukasz.kopyto@gmail.com>
2023-09-10 09:43:09 +0200

    project set-up

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    browser: true,
+  },
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/eslint-config-typescript",
+    "plugin:prettier/recommended",
+  ],
+  rules: {
+    "vue/multi-word-component-names": "off",
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+  },
+};
